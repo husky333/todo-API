a6a56c40e768e495c0e62facd930f919b10b96a0
Chris Hardin <chrishusky27@gmail.com>
2017-03-06 20:34:50 -0700

    added underscore package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -16,13 +17,9 @@ app.get('/todos', function(req, res) {
 });
 
 app.get('/todos/:id', function(req, res) {
-  var matchedTodo;
+
   var todoId = parseInt(req.params.id, 10);
-  todos.forEach((todo) => {
-    if(todo.id === todoId) {
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
   if (matchedTodo) {
     res.json(matchedTodo);
   } else {
@@ -31,8 +28,12 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function(req, res) {
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
 
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+  body.description = body.description.trim();
   body.id = todoNextId;
   todoNextId++;
   todos.push(body);
