58cab44f6dddc391c595f0d53c810a20ebe81c1c
Chris Hardin <chrishusky27@gmail.com>
2017-03-15 12:48:56 -0600

    added filter by completed

--- server.js
+++ server.js
@@ -13,7 +13,17 @@ app.get('/', function(req, res) {
 });
 
 app.get('/todos', function(req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {completed: true});
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+
+
+  res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function(req, res) {
@@ -68,7 +78,7 @@ app.put('/todos/:id', function(req,res) {
     return res.status(400).send();
   }
 
-  if (body.hasOwnProperty('description') && _.isString(body.completed) && body.description.trim().length === 0) {
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
     validAttributes.description = body.description;
   } else if (body.hasOwnProperty('description')) {
     return res.status(400).send();
