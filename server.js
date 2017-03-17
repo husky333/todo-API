9e9d6421be82ec44dd5758a03f09fa966db253c4
Chris Hardin <chrishusky27@gmail.com>
2017-03-16 22:19:54 -0600

    can delete from db

--- server.js
+++ server.js
@@ -66,14 +66,22 @@ app.post('/todos', function(req, res) {
 
 app.delete('/todos/:id', function(req,res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
 
-  if (!matchedTodo) {
-    res.status(404).json({"error": "no todo found"});
-  } else {
-    todos = _.without(todos, matchedTodo);
-    res.json(matchedTodo);
-  }
+  db.todo.destroy({
+    where: {
+      id: todoId
+    }
+  }).then((rowsDeleted) => {
+    if(rowsDeleted === 0) {
+      res.status(404).json({
+        error: 'no todo with id'
+      });
+    } else {
+      res.status(204).send();
+    }
+  }, (e) => {
+    res.status(500).send();
+  })
 });
 
 app.put('/todos/:id', function(req,res) {
