4b269187b8446f14f3bde67669373e5d4ff48d1a
Chris Hardin <chrishusky27@gmail.com>
2017-03-19 14:14:29 -0500

    finished logout

--- server.js
+++ server.js
@@ -159,6 +159,14 @@ app.post('/users/login', function(req,res) {
   });
 });
 
+app.delete('/users/login', middleware.requireAuthentication, function(req,res) {
+  req.token.destroy().then(function(){
+    res.status(204).send();
+  }, function() {
+    res.status(500).send();
+  });
+})
+
 db.sequelize.sync({force:true}).then(() => {
   app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT);
